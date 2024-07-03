fca133d35d58c6749697c40b15e81147ec80df6d
IamLite <tariel2@live.com>
2024-07-03 01:23:55 -0400

    chore: Update dependencies and add daisyui plugin

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -1,21 +1,64 @@
 'use client';
 
 import { Color } from 'next-colors';
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { FiCheck, FiCopy } from 'react-icons/fi';
 
-const ColorSwatch = ({ color, label }: { color: Color; label: string }) => (
-	<div className='flex flex-col items-center'>
-		<div
-			className='w-8 h-8 rounded-full shadow-md border border-gray-200'
-			style={{ backgroundColor: color.toHex() }}></div>
-		<span className='mt-1 text-xs'>{label}</span>
-	</div>
-);
+const FloatingElement: React.FC = () => {
+	const [copied, setCopied] = useState(false);
+
+	const handleCopy = () => {
+		navigator.clipboard.writeText('npm i next-colors');
+		setCopied(true);
+		setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+	};
+
+	return (
+		<div className='mockup-code w-1/4 my-8'>
+			<span className='text-center'>{copied ? 'Copied!' : ''}</span>
+			<pre data-prefix='$'>
+				<code>npm i next-colors</code>
+				<button
+					onClick={handleCopy}
+					className='text-white px-4 py-2 rounded-full flex items-center space-x-2 absolute top-[40%] right-2'>
+					{copied ? <FiCheck size={20} /> : <FiCopy size={20} />}
+				</button>
+			</pre>
+		</div>
+	);
+};
+
+const ColorSwatch = ({ color, label }: { color: Color; label: string }) => {
+	const [isClicked, setIsClicked] = useState(false);
+
+	const handleCopyColor = () => {
+		navigator.clipboard.writeText(color.toHex());
+		setIsClicked(true);
+		setTimeout(() => {
+			setIsClicked(false);
+		}, 500);
+	};
+
+	return (
+		<div className='flex flex-col items-center mb-2'>
+			<div
+				className={`uppercase w-14 h-14 hover:scale-150 transition-transform rounded-3xl shadow-md border border-gray-200 flex items-center justify-center cursor-pointer ${isClicked ? 'animate-pulse' : ''}`}
+				style={{ backgroundColor: color.toHex() }}
+				onClick={handleCopyColor}>
+				<span
+					className='text-xs'
+					style={{ color: color.toHsl().l > 50 ? '#000' : '#fff' }}>
+					{isClicked ? 'Copied!' : label}
+				</span>
+			</div>
+		</div>
+	);
+};
 
 const ColorSection = ({ title, colors }: { title: string; colors: Color[] }) => (
 	<div className='mb-4'>
 		<h3 className='text-sm font-semibold mb-2 text-center'>{title}</h3>
-		<div className='flex flex-wrap justify-center gap-2'>
+		<div className='flex flex-wrap justify-center'>
 			{colors.map((color, index) => (
 				<ColorSwatch
 					key={index}
@@ -61,8 +104,13 @@ const ConversionSection = ({ color }: { color: Color }) => {
 export default function Home() {
 	const [color, setColor] = useState<Color>(new Color('#916ff6'));
 	const [inputValue, setInputValue] = useState('#916ff6');
-	const [copied, setCopied] = useState(false);
-	const codeRef = useRef<HTMLPreElement>(null);
+	const [backgroundStyle, setBackgroundStyle] = useState({});
+
+	useEffect(() => {
+		setBackgroundStyle({
+			background: `radial-gradient(circle, ${color.toHex()} 0%, #1f2937 100%)`,
+		});
+	}, [color]);
 
 	const handleColorChange = (newColorValue: string) => {
 		setInputValue(newColorValue);
@@ -75,8 +123,10 @@ export default function Home() {
 	};
 
 	return (
-		<div className='min-h-screen bg-zinc-800 text-gray-200 py-8'>
-			<div className='max-w-6xl mx-auto px-4'>
+		<div
+			className='min-h-screen py-8 justify-center'
+			style={backgroundStyle}>
+			<div className='max-w-[70vw] mx-auto px-4'>
 				<div className='relative top-4 right-4 text-right'>
 					<a
 						href='https://github.com/iamlite/next-colors'
@@ -92,35 +142,36 @@ export default function Home() {
 							fill='none'
 							strokeLinecap='round'
 							strokeLinejoin='round'
-							className='text-gray-400 hover:text-white'>
+							className='text-white transition-transform hover:scale-150 duration-500'>
 							<path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
 						</svg>
 					</a>
 				</div>
 
-				<h1 className='text-2xl font-bold mb-4 text-center'>next-colors</h1>
+				<h1 className='text-2xl font-bold mb-4 text-center text-white'>next-colors Demo</h1>
 
 				<div className='mb-4 flex items-center justify-center space-x-2'>
 					<input
 						type='text'
 						value={inputValue}
 						onChange={(e) => handleColorChange(e.target.value)}
-						className='w-48 p-2 border border-gray-300 rounded text-center text-gray-800'
+						className='input max-w-xs'
 						placeholder='Enter a color'
 					/>
 					<input
 						type='color'
 						value={color.toHex()}
 						onChange={(e) => handleColorChange(e.target.value)}
-						className='w-10 h-10 rounded cursor-pointer'
+						style={{ backgroundColor: color.toHex() }}
+						className='w-10 h-10 mask mask-squircle cursor-pointer shadow'
 					/>
 				</div>
-				<p className='text-center text-sm mb-4'>
+				<p className='text-center text-sm mb-4 text-white'>
 					Enter any valid color format: HEX, RGB, HSL, HSV, CMYK, LAB, or LCH
 				</p>
 
-				<div className='flex gap-4'>
-					<div className='flex-1 bg-gray-100 text-gray-700 shadow-md rounded-lg p-4'>
+				<div className='flex flex-col md:flex-row gap-4'>
+					<div className='flex-1 bg-white bg-opacity-90 text-gray-700 shadow-md rounded-lg p-4'>
 						<ColorSection
 							title='Base Color'
 							colors={[color]}
@@ -128,9 +179,9 @@ export default function Home() {
 						<ConversionSection color={color} />
 					</div>
 
-					<div className='flex-1 bg-gray-100 text-gray-700 shadow-md rounded-lg p-4'>
+					<div className='flex-1 bg-white bg-opacity-90 text-gray-700 shadow-md rounded-lg p-4'>
 						<h2 className='text-lg font-semibold mb-4 text-center'>Color Harmonies</h2>
-						<div className='grid grid-cols-2 gap-6'>
+						<div className='grid grid-cols-2 gap-4'>
 							<ColorSection
 								title='Complementary'
 								colors={color.complementary()}
@@ -157,6 +208,9 @@ export default function Home() {
 							/>
 						</div>
 					</div>
+				</div>
+				<div className='flex justify-center'>
+					<FloatingElement />
 				</div>
 			</div>
 		</div>
--- demo/tailwind.config.ts
+++ demo/tailwind.config.ts
@@ -8,13 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
