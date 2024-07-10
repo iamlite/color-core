dc309ec219fa7f7756133c4655f0b49baaf25192
IamLite <tariel2@live.com>
2024-07-10 15:27:16 -0400

    chore: Update docs

--- demo/next.config.js
+++ demo/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/docs',
+                destination: '/docs/index.html',
+            },
+            {
+                source: '/docs/:path*',
+                destination: '/docs/:path*.html',
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
