95fe9741dbdd9dd092d0be0cc41c20d18aa9abe6
IamLite <tariel2@live.com>
2024-07-10 14:10:27 -0400

    .

--- demo/next.config.js
+++ demo/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/docs',
+                destination: '/docs/index.html',
+            },
+            {
+                source: '/docs/',
+                destination: '/docs/index.html',
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
